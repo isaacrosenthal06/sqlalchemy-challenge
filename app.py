a3c1266231fac3eae160048346dc7f55f04c042b
isaacrosenthal06 <isaacrosenthal06@gmail.com>
2022-06-25 13:17:25 -0500

    main

---
+++ app.py
@@ -0,0 +1,64 @@
+from flask import Flask, render_template, redirect, jsonify
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+import json
+import numpy as np
+import pandas as pd
+from bson import json_util
+from climate_python import prcp_dic, station_names, temp_twelve_month, start_tobs_measures
+
+engine = create_engine("sqlite:///hawaii.sqlite")
+Base = automap_base()
+
+
+# reflect the tables
+Base.prepare(engine, reflect=True)
+
+Base.classes.keys()
+
+measurement = Base.classes.measurement
+station = Base.classes.station
+
+session = Session(engine)
+
+
+
+
+
+
+app = Flask(__name__)
+
+@app.route("/")
+def welcome():
+    return (
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation<br>"
+        f"/api/v1.0/stations<br>"
+        f"/api/v1.0/tobs<br>"
+        f"/api/v1.0/<start><br>"
+        f"/api/v1.0/<start>/<end><br>"
+    )
+
+@app.route("/api/v1.0/precipitation")
+def prcp():
+    return jsonify(prcp_dic)
+
+@app.route("/api/v1.0/stations")
+def stations():
+    return json_util.dumps([datum for datum in station_names])
+
+@app.route("/api/v1.0/tobs")
+def tobs():
+    return json_util.dumps([datum for datum in temp_twelve_month])
+
+
+@app.route("/api/v1.0/<start>")
+def start(start):
+    data = start_tobs_measures(start)
+    return json_util.dumps(data)
+    
+
+
+
---
+++ climate_python.py
@@ -0,0 +1,158 @@
+
+#import matplotlib
+#style.use('fivethirtyeight')
+#import matplotlib.pyplot as plt
+#from dateutil.relativedelta import *
+import numpy as np
+import pandas as pd
+import datetime as dt
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+engine = create_engine("sqlite:///hawaii.sqlite")
+Base = automap_base()
+
+
+# reflect the tables
+Base.prepare(engine, reflect=True)
+
+Base.classes.keys()
+
+measurement = Base.classes.measurement
+station = Base.classes.station
+
+session = Session(engine)
+
+
+measurement.__dict__
+
+# Design a query to retrieve the last 12 months of precipitation data and plot the results. 
+# Starting from the most recent data point in the database. 
+
+
+
+
+# Calculate the date one year from the last date in data set.
+#twelvemonthsago = dt.datetime(2017, 8, 23) - relativedelta(months=+12)
+#print(twelvemonthsago)
+
+# Perform a query to retrieve the data and precipitation scores
+
+annual_prcp = session.query(measurement.date, measurement.prcp).\
+                  filter(measurement.date >= dt.datetime(2016, 8, 23, 0)).all()
+dates = []
+prcp = []
+for row in annual_prcp:
+    dates.append(row[0])
+    prcp.append(row[1])
+
+
+
+prcp_dic = {'Date' : dates, 'Percipitation' : prcp}
+# Save the query results as a Pandas DataFrame and set the index to the date column
+prcp_df = pd.DataFrame(prcp_dic)
+
+prcp_df.head()
+
+
+
+# Sort the dataframe by date
+
+sorted_prcp_df = prcp_df.sort_values(by='Date')
+#sorted_prcp_df.plot(kind='line', x='Date', y='Percipitation', grid = True, ylabel = "Inches", rot = 90, title = "Last Twelve Months: Percipitation in Hawaii")
+
+
+# Use Pandas Plotting with Matplotlib to plot the data
+
+#plt.plot(sorted_prcp_df.iloc[:,0], sorted_prcp_df.iloc[:,1], label = "Percipitation")
+#plt.legend(loc='best')
+
+#plt.title('Percipitation in Hawaii for Last Twelve Months')
+#plt.xlabel('Inches')
+#plt.xticks(np.arange())
+#plt.ylabel('Date')
+
+#plt.show()
+
+
+
+total_stations = session.query(measurement, station).\
+    filter(measurement.station == station.station).\
+    group_by(station.id).count()
+#print(total_stations)
+
+station_names = session.query(station.name).\
+    filter(measurement.station == station.station).\
+    group_by(station.id).all()
+
+
+test = session.query(station.name, func.count(measurement.station)).\
+    filter(measurement.station == station.station).\
+    group_by(station.name).\
+    order_by(func.count(station.id).desc()).all()
+# List the stations and the counts in descending order.
+#for row in test:
+ #   print(row)
+
+
+
+highest_temp = session.query(func.max(measurement.tobs)).\
+    filter(measurement.station == station.station).\
+    filter(station.name == 'WAIHEE 837.5, HI US').all()
+##print(highest_temp)
+
+average_temp = session.query(func.avg(measurement.tobs)).\
+    filter(measurement.station == station.station).\
+    filter(station.name == 'WAIHEE 837.5, HI US').all()
+#print(average_temp)
+
+
+temp_twelve_month = session.query(measurement.tobs).\
+    filter(measurement.station == station.station).\
+    filter(measurement.date >= dt.datetime(2016, 8, 23, 0)).\
+    filter(station.name == 'WAIHEE 837.5, HI US').all()
+
+#temp_twelve_month
+
+#temp_df = pd.DataFrame(temp_twelve_month)
+#temp_df = temp_df.rename(columns={0:'Temperature'})
+#temp_df.head()
+
+#temp_df['Frequency'] = temp_df.groupby(['Temperature'])['Temperature'].transform('count')
+#temp_df.head()
+#index = pd.Index(range(0, 25))
+
+#hist_data = temp_df.value_counts()
+#hist_data
+
+
+#def column(matrix, i):
+    #return [row[i] for row in matrix]
+
+#frequencies = column(hist_data, 1)
+
+
+def start_tobs_measures(startdate):
+    return
+    session.query(func.min(measurement.tobs), func.max(measurement.tobs), func.avg(measurement.tobs)).\
+    filter(measurement.date >= startdate).all()
+
+print(start_tobs_measures(2016-8-3))
+
+#len(temp_df.value_counts())
+
+
+#hist_temp_df = pd.DataFrame(hist_data)
+
+#hist_temp_df.head()
+
+
+
+bins = 12
+#plt.hist(temp_df['Temperature'], bins, density= 1)
+#plt.xlabel('Temperature')
+#plt.ylabel('Frequency')
+session.close()
